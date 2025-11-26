73edfbab3f3375f9aea160930c3f74184d0717cf
Samir Aoulad Amar <aouladamarsamirr2@gmail.com>
2025-11-27 00:06:41 +0100

    foat(construct):next js folders before start project pages ,

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import './globals.css'
-export default function App() {
+"use client"
+
+import "./globals.css"
+
+export default function Page() {
   return (
-    <div className="app"> 
-    
+    <div className="mainApp">
+      <video
+        autoPlay
+        loop
+        muted
+        className="videoBackground"
+        onPlay={(e) => {
+          e.currentTarget.playbackRate = 0.4
+        }}
+      >
+        <source src="/videobackground/5.mp4" type="video/mp4" />
+      </video>
     </div>
   )
 }
